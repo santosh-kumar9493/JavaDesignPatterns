4a45bdf8a428757d0859c93c91e38902d624471a
santosh <padavalsantosh@gmail.com>
2024-07-27 20:25:10 +0530

    changes

--- src/designpatterns/command/Client.java
+++ src/designpatterns/command/Client.java
@@ -2,6 +2,7 @@ package designpatterns.command;
 
 
 // This pattern is used to solving the do / undo kind of problems , every operation was made to a command
+// For refernce : https://notebook.zohopublic.in/public/notes/u3i1s7022b343ebda42819272312415abb520
 public class Client {
     public static void main(String[] args) {
         AirConditionar airConditionar = new AirConditionar();
