6dc74661a4f2e370db93290b129be366b29ac532
santosh-kumar9493 <61039032+santosh-kumar9493@users.noreply.github.com>
2025-03-27 12:21:49 +0530

    explanation to factory

--- src/designpatterns/factory/Client.java
+++ src/designpatterns/factory/Client.java
@@ -14,3 +14,24 @@ public class Client {
         andriodMenu.showMenu();
     }
 }
+
+/*
+This is example of abstract factory which is factory of factory.
+
+First let us understand what is factory and why we need factory?
+
+Factory -> This is similar to real world factory that helps us creating objects
+
+Why we need factory?
+Let’s say you are building a game where there are different types of characters like knights,
+ archers, and wizards. Instead of directly creating each character every time you need one 
+ (which could lead to messy code), you use a factory. You tell the factory, "I need a character,"
+  and the factory decides whether to create a knight, archer, or wizard based on certain
+   conditions
+
+
+Abstract factory?
+Similar to factory, in the above example , AndriodFactory and IOSFactory are used based on condition,
+we will have FactoryHelper which helps in getting the type of factory we want.
+
+*/
